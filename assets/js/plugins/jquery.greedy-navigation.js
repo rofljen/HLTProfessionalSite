553e9ac3962aa5d81cc88162615204b1165d8af4
rofljen <rofljen@gmail.com>
2025-03-21 10:07:14 -0700

    Update jquery.greedy-navigation.js

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -1,13 +1,10 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
 $(document).ready(function() {
-    // Navigation Overflow Handling
     var $nav = $('#site-nav');
     var $btn = $('#site-nav button');
     var $vlinks = $('#site-nav .visible-links');
     var $hlinks = $('#site-nav .hidden-links');
     var breaks = [];
-  
+    
     function updateNav() {
         var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
         if ($vlinks.width() > availableSpace) {
@@ -31,42 +28,10 @@ $(document).ready(function() {
             updateNav();
         }
     }
-  
-    $(window).resize(function() {
-        updateNav();
-    });
-  
-    $btn.on('click', function() {
-        $hlinks.toggleClass('hidden');
-        $(this).toggleClass('close');
-    });
-  
-    if ($nav.length && $vlinks.length && $hlinks.length) {
-        updateNav();
-    }
-
-    // Mobile Menu Toggle
-    const menuToggle = $('.menu-toggle');
-    const navLinks = $('.nav-links');
-
-    menuToggle.click(function(e) {
-        e.stopPropagation();
-        navLinks.toggleClass('show');
-    });
-
-    $(document).click(function(event) {
-        if (!navLinks.is(event.target) && navLinks.has(event.target).length === 0) {
-            navLinks.removeClass('show');
-        }
-    });
-
-    $('.nav-links a').click(function() {
-        navLinks.removeClass('show');
-    });
-
-    // Dropdown Toggle
+    
+    // Mobile Dropdown Toggle
     $("#dropdown-btn").click(function(event) {
-        event.stopPropagation(); // Prevents closing when clicking inside
+        event.stopPropagation(); 
         $("#dropdown-menu").toggle();
     });
 
@@ -75,12 +40,12 @@ $(document).ready(function() {
         $("#dropdown-menu").hide();
     });
 
-    // Ensure menu closes on resize if window width increases
     $(window).resize(function() {
+        updateNav();
         if ($(window).width() > 768) {
-            navLinks.removeClass('show');
             $("#dropdown-menu").hide();
         }
     });
+
+    updateNav();
 });
-</script>
